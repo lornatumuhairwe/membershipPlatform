4124ed9cf604da9ca914b19b5f2d1e9e69aeb26a
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2019-06-17 16:50:52 +0300

    Error handling.

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -2,5 +2,8 @@ class PlanController < ApplicationController
   def members
     plan = Plan.find(params[:id])
     render json: plan.members
+
+  rescue ActiveRecord::RecordNotFound
+    render json: 'Plan not found', status: 404
   end
 end
--- spec/requests/plans_spec.rb
+++ spec/requests/plans_spec.rb
@@ -2,18 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'plans requests', type: :request do
   let(:plan1) { create(:plan) }
+  let(:plan_id) { plan1.id }
   let(:plan2) { create :plan, :time_limited }
   let!(:plan1_members) { create_list :member, 5, plan: plan1}
   let!(:plan2_members) { create_list :member, 2, plan: plan2}
 
   describe 'GET /plan/:id/members' do
-    before { get "/plan/#{plan1.id}/members" }
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:ok)
+    before { get "/plan/#{plan_id}/members" }
+    context 'when plan exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns members of plan1' do
+        expect(JSON.parse(response.body).size).to eq(5)
+      end
     end
 
-    it 'returns members of plan1' do
-      expect(JSON.parse(response.body).size).to eq(5)
+    context 'when plan does not exist' do
+      let(:plan_id) { 0 }
+
+      it 'should return status code 404' do
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
